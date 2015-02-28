2b8ef4080117eb96436b8da5df930729417efa75
Piotr Górak <piotrgorak93@gmail.com>
2015-02-28 02:36:43 +0100

    OOP

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
  * Date: 2015-02-28
  * Time: 00:39
  */
+require_once('models/First.php');
+require_once('models/Second.php');
+require_once('models/Third.php');
 include ('views/header.html');
 include ('views/home.html');
 new First();
